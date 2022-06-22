3c8a97134e84a24ced9921f78f10b58e760f8a4b
Nazarii Melnyk <nazarii.melnyk@rolique.io>
2022-06-22 21:16:47 +0300

    added styled components

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,54 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import styled from "styled-components/native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+const RedView = styled.View`
+  background-color: grey;
+`;
+
+const Container = styled.View`
+  display: flex;
+`;
+
+const Title = styled.Text`
+  font-size: 20;
+  font-weight: 600;
+  color: white;
+`;
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <RedView>
+      <Title>Tab One</Title>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <Container>
+        <Title>Test App</Title>
+      </Container>
+    </RedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
